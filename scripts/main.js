6ba86e951d4bfdb29b1663d5a75d3f12120fb003
NedLandy <matheus.v4200@gmail.com>
2020-06-12 19:49:57 -0300

    Criação de janela genérica

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+console.log("running....")
+
+const electron = require("electron")
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const path = require("path")
+const url = require("url")
+
+let window
+
+function createWindow() {
+    window = new BrowserWindow()
+    window.loadURL(url.format({
+        pathname: path.join(__dirname,'index.html'),
+        protocol: 'file',
+        slashes: true
+    }))
+
+    window.on('closed',()=>{
+        win = null
+    })
+}
+
+app.on('ready',createWindow)
+
+app.on('window-all-closed',()=>{
+    if(process.platform !== 'darwin'){
+        app.quit()
+    }
+})
+
+app.on('activate',()=>{
+    if(win === null){
+        createWindow()
+    }
+})
