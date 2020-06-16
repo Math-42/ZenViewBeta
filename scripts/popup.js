73535b48414d64dd989695adc1ec6bc7dd6e0cb0
NedLandy <matheus.v4200@gmail.com>
2020-06-16 20:36:49 -0300

    refatoração para módulo

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,21 +1,48 @@
-function openPopup(menu){
-  document.getElementById("popup_background").style.display = "block";
-}
-
-function closePopup(){
-  document.getElementById("popup_background").style.display = "none";
-}
-
-function openMenu(id, menuName) {
-  let i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+module.exports = class popUpMenu{
+  constructor(){
+    this.tablinks = {}
+    this.menus = {}
   }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+
+  setTablinks(tabLinks){
+    console.log("ok")
+    this.tablinks = tabLinks;
   }
-  document.getElementById(id).className += " active"
-  document.getElementById(menuName).style.display = "block"; 
+
+  setMenus(menus){
+    console.log("ok")
+    this.menus = menus
+  }
+
+  openPopup(menu){
+    document.getElementById("popup_background").style.display = "block";
+    this.openMenu(menu+'_tablink',menu);
+  }
+
+  closePopup(){
+    document.getElementById("popup_background").style.display = "none";
+  }
+
+  loadMenuFunction(menuName){
+    console.log(this.menus[menuName].onLoadFunction)
+    if(this.menus[menuName].onLoadFunction !== undefined){
+      this.menus[menuName].onLoadFunction()
+    }
+  }
+
+  openMenu(id, menuName){
+    console.log(this.menus)
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(id).className += " active"
+    document.getElementById(menuName).style.display = "block";
+    this.loadMenuFunction(menuName)
+  }
 }
--- scripts/sideMenu.js
+++ scripts/sideMenu.js
@@ -1,13 +1,15 @@
 //Muda a posicao do menu lateral conforme o usuário pressiona o botão
-function openSideMenu() {
-  if(document.getElementById("side_menu").style.width ==="60px"){
-    document.getElementById("side_menu").style.width = "280px";
-    document.getElementById("main").style.marginLeft = "280px";
-    document.getElementById("side_tab_menu").style.display = "block";
-    
-  }else{
-    document.getElementById("side_tab_menu").style.display = "none";
-    document.getElementById("side_menu").style.width = "60px";
-    document.getElementById("main").style.marginLeft = "60px";
-  }
+module.exports = class SideMenu{
+    changeSideMenu(){
+        if(document.getElementById("side_menu").style.width ==="60px"){
+            document.getElementById("side_menu").style.width = "280px";
+            document.getElementById("main").style.marginLeft = "280px";
+            document.getElementById("side_tab_menu").style.display = "block";
+            
+        }else{
+            document.getElementById("side_tab_menu").style.display = "none";
+            document.getElementById("side_menu").style.width = "60px";
+            document.getElementById("main").style.marginLeft = "60px";
+        }
+    }
 }
