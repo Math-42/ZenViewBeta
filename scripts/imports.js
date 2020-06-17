c4771239e3d7007dee76b8ffe0ba71b2f3cf946a
NedLandy <matheus.v4200@gmail.com>
2020-06-17 02:59:51 -0300

    Menu de edição dos data views

--- scripts/imports.js
+++ scripts/imports.js
@@ -1,6 +1,7 @@
 const NewDataView = require('../interface/popupMenus/NewDataView')
 const DataViewsList = require('../interface/popupMenus/DataViewsList')
+const DataViewsEdit = require('../interface/popupMenus/DataViewsEdit')
 
-let modules = {NewDataView, DataViewsList};
+let modules = {NewDataView, DataViewsList,DataViewsEdit};
 
 module.exports = modules
