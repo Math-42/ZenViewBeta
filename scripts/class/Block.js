9909995060c3c81e5f827b1d282495202cbc38ee
NedLandy <matheus.v4200@gmail.com>
2020-08-21 18:33:12 -0300

    Bloco para fins genéricos

--- scripts/class/Block.js
+++ scripts/class/Block.js
@@ -1,61 +1,37 @@
-module.exports =  class Block{
-    constructor(plotLib){
+
+const ElementResize = require('javascript-detect-element-resize');
+const PlotlyBlock = require('./blocks/plotlyBlock');
+module.exports = class Block {
+    constructor(id,plotLib) {
         this.plotLib = plotLib;
-        this.id;
+        this.id = id;
         this.width = 4;
-        this.height = 2;
+        this.height = 3;
         this.inputs = [];
+        this.plot = {};
     }
-    openPlotEditingMenu(){
-        console.log("menu de edição de plots aberto");
-        if(document.getElementById("editing_menu").style.display ==="block"){
-            document.getElementById("editing_menu").style.display = "none";
-            document.getElementById("main").style.marginRight = "0px";
-            
-        }else{
-            document.getElementById("editing_menu").style.display = "block";
-            document.getElementById("editing_menu").style.width = "22%";
-            document.getElementById("main").style.marginRight = "22%";
-        }
-    }
-    plotlyHtmlComponent(id){
-        id = (id === undefined)? this.id+"_plot": id;
-        let plotPlotly = document.createElement('div');
-        plotPlotly.id = id;
-        plotPlotly.className = "grid-stack-item-content";
-        plotPlotly.style.backgroundColor = "white";
-        return plotPlotly;
-    }
-    packageSelector(id){
+    packageSelector() {
         let newPlot;
-        if(this.plotLib === "Plotly"){
-            newPlot = this.plotlyHtmlComponent(id);
-        }else{
-            newPlot = document.createElement('div');
-            newPlot.id = id;
-            newPlot.className = "grid-stack-item-content";
-            newPlot.style.backgroundColor = "whitesmoke";
+        if (this.plotLib === "Plotly") {
+            this.plot = new PlotlyBlock(this.id);
+            newPlot = this.plot.htmlComponent();
         }
-        newPlot.ondblclick = ()=>{this.openPlotEditingMenu()};
+        newPlot.ondblclick = () => {
+            mainwindow.dispatchEvent('openEditingMenu', {
+                "block": this
+            });
+        };
         return newPlot;
     }
-    setAutoResize(){
-        if(this.plotLib === "Plotly"){
-            let chart = document.getElementById(plot.id);
-            addResizeListener(chart,()=>{
-                Plotly.relayout(chart.id+"_plot", {
-                    'xaxis.autorange': true,
-                    'yaxis.autorange': true
-                });
-            })
-        }
+    init(){
+        console.log("inicializando bloco "+this.id);
+        this.plot.init();
     }
-    plotHtmlComponent(id){
-        id = (id === undefined)? this.id: id;
+    plotHtmlComponent() {
         let newWidget = document.createElement("div");
-        newWidget.id = id;
+        newWidget.id = this.id;
         newWidget.className = "grid-stack-item";
-        newWidget.appendChild(this.packageSelector(id));
+        newWidget.appendChild(this.packageSelector());
         return newWidget;
     }
 }
