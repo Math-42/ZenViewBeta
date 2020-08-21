5e8b4d3374dbffe5a30d2c8772dc2cceeb827e7e
NedLandy <matheus.v4200@gmail.com>
2020-08-21 18:34:58 -0300

    Concertando erro ao salvar nome dos dashboards

--- interface/popupMenus/NewDashBoard.js
+++ interface/popupMenus/NewDashBoard.js
@@ -21,10 +21,51 @@ module.exports = class NewDashBoard{
                     <script language="JavaScript" charset="utf-8" src="../../scripts/dashBoard.js"></script>
                     <script src="../../node_modules/gridstack/dist/gridstack.all.js"></script>
                     <script src='../../node_modules/javascript-detect-element-resize/detect-element-resize.js'></script>
+                    <style>
+                        body,html{
+                            padding: 0px;
+                            margin: 0px;
+                            width: 100%;
+                            height: 100%;
+                        }
+                        #editing_menu {
+                            height: 100%;
+                            width: 22%;
+                            z-index: 1;
+                            position: fixed;
+                            top: 0;
+                            right: 0;
+                            background-color: #1c1e23;
+                            overflow-x: hidden;
+                            padding-top: 60px;
+                            transition: 0.1s;
+                            display: none;
+                        }
+                        ::-webkit-scrollbar {
+                        width: 7px;
+                        }
+
+                        /* Track */
+                        ::-webkit-scrollbar-track {
+                        background: #f1f1f1; 
+                        }
+                        
+                        /* Handle */
+                        ::-webkit-scrollbar-thumb {
+                        background: #888; 
+                        }
+
+                        /* Handle on hover */
+                        ::-webkit-scrollbar-thumb:hover {
+                        background: #555; 
+                        }
+                    </style>
                 </head>
                 <body>
-                    <div class="grid-stack" >
+                    <div class="grid-stack h-100" id="main">
                         
+                    </div>
+                    <div class="h-100" id="editing_menu">
                     </div>
 
                     <script type="text/javascript">
@@ -52,7 +93,7 @@ module.exports = class NewDashBoard{
             alert("The number of inputs must be between 1 and 30");
             return;
         }
-        let dashBoardA = new DashBoard(name.value,parseInt(this.inputs.value),this.description.value);
+        let dashBoardA = new DashBoard(this.name.value,parseInt(this.inputs.value),this.description.value);
         this.dashBoards = fs.readdirSync('dashBoards');
         if(!this.dashBoards.includes(this.name.value)){
             dashBoardA.inputs = [];
@@ -62,10 +103,11 @@ module.exports = class NewDashBoard{
                 newInput.operation = "col"+i;
                 dashBoardA.inputs.push(newInput);
             }
+            console.log(dashBoardA);
             fs.mkdirSync(`dashBoards/${this.name.value}`);
             fs.writeFileSync(`dashBoards/${this.name.value}/${this.name.value}.json`,JSON.stringify(dashBoardA,null,"\t"));
             fs.writeFileSync(`dashBoards/${this.name.value}/${this.name.value}.html`,this.newHTML(this.name.value));
-            mainwindow.dispatchEvent('LoadNewTab',{"name":this.name.value,"context": "edit_show"});
+            mainwindow.dispatchEvent('LoadNewDashBoard',{"name":this.name.value,"context": "edit_show"});
             this.name.value = "";
             this.inputs.value = "";
             this.description.value = "";
