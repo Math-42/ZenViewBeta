1125517d2a97a297fe5fbdb3c88b0c0dd77d9eb9
NedLandy <matheus.v4200@gmail.com>
2020-08-21 21:01:36 -0300

    Removendo gerador de html

--- interface/popupMenus/NewDashBoard.js
+++ interface/popupMenus/NewDashBoard.js
@@ -11,71 +11,6 @@ module.exports = class NewDashBoard{
         this.inputs
         this.description
     }
-    newHTML(){
-        return `<!DOCTYPE html>
-            <html>
-                <head>
-                    <link rel="stylesheet" type="text/css" href="../../node_modules/bootstrap/dist/css/bootstrap.css" />
-                    <link rel="stylesheet" href="../../node_modules/gridstack/dist/gridstack.min.css" />
-                    <script type="text/javascript" charset="utf-8" src="../../node_modules/plotly.js/dist/plotly.js"></script>
-                    <script language="JavaScript" charset="utf-8" src="../../scripts/dashBoard.js"></script>
-                    <script src="../../node_modules/gridstack/dist/gridstack.all.js"></script>
-                    <script src='../../node_modules/javascript-detect-element-resize/detect-element-resize.js'></script>
-                    <style>
-                        body,html{
-                            padding: 0px;
-                            margin: 0px;
-                            width: 100%;
-                            height: 100%;
-                        }
-                        #editing_menu {
-                            height: 100%;
-                            width: 22%;
-                            z-index: 1;
-                            position: fixed;
-                            top: 0;
-                            right: 0;
-                            background-color: #1c1e23;
-                            overflow-x: hidden;
-                            padding-top: 60px;
-                            transition: 0.1s;
-                            display: none;
-                        }
-                        ::-webkit-scrollbar {
-                        width: 7px;
-                        }
-
-                        /* Track */
-                        ::-webkit-scrollbar-track {
-                        background: #f1f1f1; 
-                        }
-                        
-                        /* Handle */
-                        ::-webkit-scrollbar-thumb {
-                        background: #888; 
-                        }
-
-                        /* Handle on hover */
-                        ::-webkit-scrollbar-thumb:hover {
-                        background: #555; 
-                        }
-                    </style>
-                </head>
-                <body>
-                    <div class="grid-stack h-100" id="main">
-                        
-                    </div>
-                    <div class="h-100" id="editing_menu">
-                    </div>
-
-                    <script type="text/javascript">
-                        let thisDashBoard = new DashBoard();
-
-                        thisDashBoard.init()
-                    </script>
-                </body>
-            </html>`
-    }
     onLoadFunction(){
         this.name = document.getElementById("view_name");
         this.inputs = document.getElementById("view_inputs");
@@ -106,7 +41,6 @@ module.exports = class NewDashBoard{
             console.log(dashBoardA);
             fs.mkdirSync(`dashBoards/${this.name.value}`);
             fs.writeFileSync(`dashBoards/${this.name.value}/${this.name.value}.json`,JSON.stringify(dashBoardA,null,"\t"));
-            fs.writeFileSync(`dashBoards/${this.name.value}/${this.name.value}.html`,this.newHTML(this.name.value));
             mainwindow.dispatchEvent('LoadNewDashBoard',{"name":this.name.value,"context": "edit_show"});
             this.name.value = "";
             this.inputs.value = "";
