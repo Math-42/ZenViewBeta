f97f24c2a3bbf4918ecdad4a3f94148e38107bde
NedLandy <matheus.v4200@gmail.com>
2020-08-24 18:49:48 -0300

    Ação de remover bloco e ler dashboard do json

--- scripts/class/DashBoard.js
+++ scripts/class/DashBoard.js
@@ -1,6 +1,7 @@
 const Block = require('./Block')
 const GridStack = require('gridstack/dist/gridstack.all')
 const ElementResize = require('javascript-detect-element-resize');
+const Input = require('./Input');
 
 
 module.exports = class DashBoard {
@@ -11,37 +12,76 @@ module.exports = class DashBoard {
         this.desc = desc;
         this.inputs = {};
         this.blocks = []
+        this.editing = true;
         this.gridStack;
         this.activeContext;
+        this.ids = 1;
 
     }
     loadFromJson(dashBoardJson) {
         this.name = dashBoardJson.name;
         this.qtdInputs = dashBoardJson.qtdInputs;
         this.desc = dashBoardJson.desc;
+        this.desc = dashBoardJson.desc;
+
+        let tempBlocks = dashBoardJson.blocks;
+
+        tempBlocks.forEach(block => {
+            let tempBlock = new Block(this.ids++, block.plotLib)
+            tempBlock.loadFromJson(block)
+            this.blocks.push(tempBlock);
+        });
+
+        let inputs = [];
+
+        dashBoardJson.inputs.forEach(input => {
+            inputs.push(new Input(input.name, input.operation));
+        });
+
+        this.inputs = inputs
     }
     init() {
+
         this.gridStack = GridStack.init({
             float: true,
             column: 12,
-            animate: true,
+            animate: true
 
         });
+
+        if(this.editing){
+            this.gridStack.enable('.grid-stack-item', true);
+        }else{
+            this.gridStack.disable('.grid-stack-item', false);
+        }
+
+        this.clear();
+
+        this.blocks.forEach(block => {
+            this.gridStack.addWidget(block.plotHtmlComponent(), block);
+            block.init(this.editing);
+        })
     }
     clear() {
         this.gridStack.removeAll();
     }
+    removeWidget(block) {
+        let widget = document.getElementById(block.id);
+        this.gridStack.removeWidget(widget)
+        for (let i = 0; i <= this.blocks.length; i++) {
+            if (block === this.blocks[i]) {
+                this.blocks.splice(i, 1);
+                break;
+            }
+        }
+    }
     addNewWidget(newBlock) {
-        newBlock = (newBlock === undefined)? new Block(this.blocks.length + 1,"Plotly"): newBlock;
+        newBlock = (newBlock === undefined) ? new Block(this.ids++, "Plotly") : newBlock;
         newBlock.height = (newBlock.height === undefined) ? 3 : newBlock.height;
         newBlock.width = (newBlock.width === undefined) ? 4 : newBlock.width;
         this.blocks.push(newBlock);
-        let widget = {
-            autoPosition: true,
-            width: newBlock.width,
-            height: newBlock.height
-        };
-        this.gridStack.addWidget(newBlock.plotHtmlComponent(), widget);
-        newBlock.init();
+        this.gridStack.addWidget(newBlock.plotHtmlComponent(), newBlock);
+
+        newBlock.init(this.editing);
     };
 }
