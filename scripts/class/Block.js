11927d7a46ae8eb3ca4479db7f175e5f45769b1a
NedLandy <matheus.v4200@gmail.com>
2020-08-31 16:16:57 -0300

    Bloco contem todos os tipos de plot, porem só inicializa quando necessário

--- scripts/class/Block.js
+++ scripts/class/Block.js
@@ -1,38 +1,54 @@
-
 const ElementResize = require('javascript-detect-element-resize');
-const lineChartPlotlyBlock = require('./blocks/lineChartPlotlyBlock');
+const LineChartPlotlyBlock = require('./blocks/lineChartPlotlyBlock');
+const IndicatorPlotlyBlock = require('./blocks/indicatorPlotlyBlock');
 module.exports = class Block {
-    constructor(id,plotLib) {
-        this.plotLib = plotLib;
+    constructor(id, type) {
+        this.plotLib = type;
         this.id = id;
         this.width = 4;
         this.height = 3;
         this.x;
         this.y;
         this.editing = true;
-        if (this.plotLib === "Plotly") {
-            this.plot = new lineChartPlotlyBlock(this.id);
-        }else{
-            this.plot = {}
+        this.currentPlot;
+        this.plotTypes;
+        this.type;
+        this.plots = {
+            Plotly: {
+                lineChart: undefined,
+                indicator: undefined
+            },
+            Threejs: undefined
         }
+        this.setPlotType(type);
     }
-    loadFromJson(BlockJson){
+    setPlotType(type) {
+        if (type.main === "Plotly") {
+            if (type.sub === "lineChart") {
+                this.plots.Plotly.lineChart = (this.plots.Plotly.lineChart === undefined) ? new LineChartPlotlyBlock(this.id) : this.plots.Plotly.lineChart;
+                this.currentPlot = this.plots.Plotly.lineChart
+            } else if (type.sub === "indicator") {
+                this.plots.Plotly.indicator = (this.plots.Plotly.indicator === undefined) ? new IndicatorPlotlyBlock(this.id) : this.plots.Plotly.indicator;
+                this.currentPlot = this.plots.Plotly.indicator
+            }
+        }
+        this.type = type;
+    }
+    loadFromJson(BlockJson) {
         this.width = BlockJson.width;
         this.height = BlockJson.height;
         this.x = BlockJson.x;
         this.y = BlockJson.y;
         this.plotLib = BlockJson.plotLib;
         this.id = BlockJson.id
-        if (this.plotLib === "Plotly") {
-            this.plot = new lineChartPlotlyBlock(this.id);
-            this.plot.loadFromJson(BlockJson.plot);
-        }
+        this.setPlotType(BlockJson.type);
+        this.currentPlot.loadFromJson(BlockJson.currentPlot);
     }
-    init(editing){
+    init(editing) {
         this.editing = editing;
-        console.log("inicializando bloco "+this.id);
-        this.plot.init(this.editing);
-        if(this.editing){
+        console.log("inicializando bloco " + this.id);
+        this.currentPlot.init(this.editing);
+        if (this.editing) {
             document.getElementById(this.id).ondblclick = () => {
                 mainwindow.dispatchEvent('openEditingMenu', {
                     "block": this
@@ -40,11 +56,11 @@ module.exports = class Block {
             };
         }
     }
-    load(editing){
+    load(editing) {
         this.editing = editing;
-        console.log("Carregando bloco "+this.id);
-        this.plot.load(this.editing);
-        if(this.editing){
+        console.log("Carregando bloco " + this.id);
+        this.currentPlot.load(this.editing);
+        if (this.editing) {
             document.getElementById(this.id).ondblclick = () => {
                 mainwindow.dispatchEvent('openEditingMenu', {
                     "block": this
@@ -56,7 +72,7 @@ module.exports = class Block {
         let newWidget = document.createElement("div");
         newWidget.id = this.id;
         newWidget.className = "grid-stack-item";
-        newWidget.appendChild(this.plot.htmlComponent());
+        newWidget.appendChild(this.currentPlot.htmlComponent());
         return newWidget;
     }
 }
