47bef9776414914cb38be1deb73ccc0aea7c8224
NedLandy <matheus.v4200@gmail.com>
2020-08-31 16:27:28 -0300

    Novo tipo de menu de edição usando menu builder

--- scripts/class/menus/plotlyEditingMenu.js
+++ scripts/class/menus/plotlyEditingMenu.js
@@ -1,117 +1,68 @@
 const MenuBuilder = require('./menuBuilder')
 const LineChartMenu = require('./plotly/lineChartMenu')
+const indicatorMenu = require('./plotly/indicatorMenu')
 module.exports = class PlotlyEditingMenu {
     constructor() {
         this.plot
-        this.colors = ["#ff0000", "#0000ff", "#ffa500", "#800080", "#008000", "#4b0082"]
-        this.colorCount = 0;
-        this.lineChartMenu = new LineChartMenu;
-        this.indicatorChartOptions;
+        this.block
+        this.menu
+        this.isInitialized = false;
+        this.lineChartMenu = new LineChartMenu();
+        this.indicatorMenu = new indicatorMenu();
         this.tridChartOptions;
         this.tableChartOptions;
         this.currentMenu;
     }
-    menuSetup() {
-        return {
-            "name": "menu_config_plot",
-            "title": "Configurações do pacote",
-            "id": `Plotly`,
-            "fields": [
-                {
-                    "type": "selectInput",
-                    "title": "Tipo de plot:",
-                    "id": `tipoDePlot`,
-                    "options":[
-                        {
-                            "text":"Linhas",
-                            "value": 1
-                        },
-                        {
-                            "text":"Tabela",
-                            "value": 2
-                        },
-                        {
-                            "text":"Indicador",
-                            "value": 3
-                        },
-                        {
-                            "text":"3D",
-                            "value": 3
-                        }
-                    ]
-                },
-                {
-                    "type": "selectInput",
-                    "title": "Formato de exportação:",
-                    "id": `export`,
-                    "options":[
-                        {
-                            "text":"Png",
-                            "value": "png"
-                        },
-                        {
-                            "text":"Svg",
-                            "value": "svg"
-                        },
-                        {
-                            "text":"Jpeg",
-                            "value": "jpeg"
-                        },
-                        {
-                            "text":"Webp",
-                            "value": "webp"
-                        }
-                    ]
-                }
-            ]
+    load(block, menu) {
+        this.block = block
+        this.menu = menu
+        if (!this.isInitialized) {
+            this.init()
         }
-    }
-    load(plot) {
+
         console.log("menu plotly aberto")
-        document.getElementById("editing_menu_plotly").style.display = "block";
-        document.getElementById("subOptions_menu").style.display = "block";
-
-        this.plot = plot;
-        console.log(this.plot)
-        console.log("carregando menu de plot 2d")
-
-        this.loadMenu(this.plot.type)
+        this.loadMenu(this.block.type.sub)
     }
-    loadMenu(selection){
-        
-        let allMenus = document.getElementsByClassName("subMenuBlock")
-        for(let i=0, j= allMenus.length; i<j;i++){
-            console.log(allMenus[i])
-            allMenus[i].style.display = "none";
-        }
+    loadMenu(selection) {
+        console.log("Seleção: " + selection)
 
-        console.log(selection)
-        if(selection == 1){
-            this.currentMenu = this.lineChartMenu
-            this.currentMenu.init()
-            this.currentMenu.load(this.plot)
+        if (selection === "lineChart") {
+            if (this.block.plots.Plotly.lineChart === undefined) {
+                this.block.setPlotType({
+                    main: "Plotly",
+                    sub: "lineChart"
+                })
+                this.block.currentPlot.init();
+                this.lineChartMenu.load()
+            } else {
+                this.block.setPlotType({
+                    main: "Plotly",
+                    sub: "lineChart"
+                })
+            }
+
+        } else if (selection === "indicator") {
+            if (this.block.plots.Plotly.indicator === undefined) {
+                this.block.setPlotType({
+                    main: "Plotly",
+                    sub: "indicator"
+                })
+                this.block.currentPlot.init();
+            } {
+                this.block.setPlotType({
+                    main: "Plotly",
+                    sub: "indicator"
+                })
+                this.indicatorMenu.load()
+            }
         }
+        this.block.currentPlot.load(true)
     }
     init() {
-        let menuBlock = document.createElement('div');
-        menuBlock.className = "menuBlock"
+        this.menu.onFieldChange("editingMenuRoot.plotlyDiv.plotlyMenu.tipoDePlot", (value) => {
+            this.loadMenu(value)
+        })
 
-        let optionMenu = document.getElementById("subOptions_menu");
-        
-        let aba = MenuBuilder.build(this.menuSetup());
-        menuBlock.appendChild(aba);
-
-        let subMenuOptions = document.createElement('div');
-        subMenuOptions.id ="Plotly_subOptions"
-
-        menuBlock.appendChild(subMenuOptions);
-
-        optionMenu.appendChild(menuBlock)
-
-        document.getElementById('Plotly_tipoDePlot').onchange = () => {
-            let selection = document.getElementById("Plotly_tipoDePlot").value
-            console.log("Opção selcionada: " + selection)
-            this.loadMenu(selection);
-        }
+        this.isInitialized = true
     }
 }
