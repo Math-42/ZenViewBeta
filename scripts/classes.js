40d97fa088486679b5194123a2e2ec83d9ef62c8
NedLandy <matheus.v4200@gmail.com>
2020-09-02 19:12:19 -0300

    Refatorando e comentando

--- scripts/classes.js
+++ scripts/classes.js
@@ -1,9 +1,17 @@
+/**
+ * Lista e importa todas as classes utilitárias do programa, e as lista facilitando a utilização no futuro
+ */
 const DashBoard = require('../scripts/class/DashBoard')
 const Input = require('../scripts/class/Input')
 const Block = require('../scripts/class/Block')
 const EditingMenu = require('../scripts/class/EditingMenu')
 
 
-let classes = {DashBoard, Input, Block,EditingMenu};
+let classes = {
+    DashBoard,
+    Input,
+    Block,
+    EditingMenu
+};
 
 module.exports = classes
