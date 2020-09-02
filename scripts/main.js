5583a2ddbf3d78c84a2dc5ed03f099450a909ed4
NedLandy <matheus.v4200@gmail.com>
2020-09-02 19:09:10 -0300

    Comentando e refatorando main.js

--- scripts/main.js
+++ scripts/main.js
@@ -33,49 +33,54 @@ let mainWindowparams = {
     },
     openDevTools: debugMode
 }
-//cria uma nova janela a partir dos parametros dados
+/**
+ * cria uma nova janela a partir dos parametros dados
+ * @param {object} params objeto que descreve a nova janela
+ */
 function createWindow(params) {
     let window = new BrowserWindow(params)
     window.loadURL(url.format({
-        pathname: path.join(__dirname,params.path),
+        pathname: path.join(__dirname, params.path),
         protocol: 'file',
         slashes: true,
     }))
-    if(params.openDevTools){
+    if (params.openDevTools) {
         window.openDevTools()
     }
-    window.on('closed',()=>{
+    window.on('closed', () => {
         window = null
     })
     return window
 }
 //event listener que espera o app ser criado para criar as janelas
-app.on('ready',()=>{
+app.on('ready', () => {
     initialWindow = createWindow(initialWindowparams)
     mainWindow = createWindow(mainWindowparams)
     //apenas mostrara a janela quando estiver pronta
-    initialWindow.once('ready-to-show',()=>{
+    initialWindow.once('ready-to-show', () => {
         initialWindow.show();
     })
 })
 
 //encerra o programa se todas as janelas forem fechadas
-app.on('window-all-closed',()=>{
-    if(process.platform !== 'darwin'){
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
         app.quit()
     }
 })
 
 //caso a janela nao tenha sido criada força sua criação
-app.on('activate',()=>{
-    if(initialWindow === null){
+app.on('activate', () => {
+    if (initialWindow === null) {
         createWindow(initialWindow)
         createWindow(mainWindow)
     }
 })
 
 //listerner que avisa que o load da janela principal terminou
-ipc.on('mainLoadCompleto',() =>{
+ipc.on('mainLoadCompleto', () => {
     initialWindow.close()
-    setTimeout(()=>{mainWindow.show()},250) 
+    setTimeout(() => {
+        mainWindow.show()
+    }, 250)
 })
