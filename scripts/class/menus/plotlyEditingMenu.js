d8ab9cc6a15bd5d335339e79fb1588faed26c2dd
NedLandy <matheus.v4200@gmail.com>
2020-09-03 11:14:41 -0300

    Padronizando escrita

--- scripts/class/menus/plotlyEditingMenu.js
+++ scripts/class/menus/plotlyEditingMenu.js
@@ -1,11 +1,10 @@
-const MenuBuilder = require('./menuBuilder')
-const LineChartMenu = require('./plotly/lineChartMenu')
-const indicatorMenu = require('./plotly/indicatorMenu')
+const LineChartMenu = require("./plotly/lineChartMenu");
+const indicatorMenu = require("./plotly/indicatorMenu");
 module.exports = class PlotlyEditingMenu {
     constructor() {
-        this.plot
-        this.block
-        this.menu
+        this.plot;
+        this.block;
+        this.menu;
         this.isInitialized = false;
         this.lineChartMenu = new LineChartMenu();
         this.indicatorMenu = new indicatorMenu();
@@ -14,55 +13,55 @@ module.exports = class PlotlyEditingMenu {
         this.currentMenu;
     }
     load(block, menu) {
-        this.block = block
-        this.menu = menu
+        this.block = block;
+        this.menu = menu;
         if (!this.isInitialized) {
-            this.init()
+            this.init();
         }
-
-        console.log("menu plotly aberto")
-        this.loadMenu(this.block.type.sub)
+        this.loadMenu(this.block.type.sub);
     }
     loadMenu(selection) {
-        console.log("Seleção: " + selection)
+        console.warn("Seleção: " + selection);
 
         if (selection === "lineChart") {
             if (this.block.plots.Plotly.lineChart === undefined) {
                 this.block.setPlotType({
                     main: "Plotly",
                     sub: "lineChart"
-                })
+                });
                 this.block.currentPlot.init();
-                this.lineChartMenu.load()
             } else {
                 this.block.setPlotType({
                     main: "Plotly",
                     sub: "lineChart"
-                })
+                });
             }
+            this.lineChartMenu.load(this.block.currentPlot, this.menu);
 
         } else if (selection === "indicator") {
             if (this.block.plots.Plotly.indicator === undefined) {
                 this.block.setPlotType({
                     main: "Plotly",
                     sub: "indicator"
-                })
+                });
                 this.block.currentPlot.init();
             } {
                 this.block.setPlotType({
                     main: "Plotly",
                     sub: "indicator"
-                })
-                this.indicatorMenu.load()
+                });
+
             }
+            this.indicatorMenu.load(this.block.currentPlot, this.menu);
         }
-        this.block.currentPlot.load(true)
+        this.block.currentPlot.load(true);
     }
     init() {
-        this.menu.onFieldChange("editingMenuRoot.plotlyDiv.plotlyMenu.tipoDePlot", (value) => {
-            this.loadMenu(value)
-        })
+        this.menu.onFieldChange("plotSelector", (value) => {
+            console.warn("Campo de tipo de plot alterado, selecionado: " + value);
+            this.loadMenu(value);
+        });
 
-        this.isInitialized = true
+        this.isInitialized = true;
     }
-}
+};
