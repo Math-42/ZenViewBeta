12a268b060c2e1edba209eb30e8affcf311626a3
NedLandy <matheus.v4200@gmail.com>
2020-09-11 00:27:06 -0300

    refatorando código

--- interface/popupMenus/DashBoardsList.js
+++ interface/popupMenus/DashBoardsList.js
@@ -1,44 +1,47 @@
-const fs = require('fs')
+const fs = require("fs");
 
-module.exports = class DashBoardsList{
-    constructor(){
+module.exports = class DashBoardsList {
+    constructor() {
         this.dashBoards = [];
     }
-    onLoadFunction(){
-        this.dashBoards = fs.readdirSync('dashBoards');
-        this.postList(this.dashBoards)
+    onLoadFunction() {
+        this.dashBoards = fs.readdirSync("dashBoards");
+        this.postList(this.dashBoards);
     }
-    filter(){
-        this.dashBoards = fs.readdirSync('dashBoards');
-        let dashBoardsList = []
-        let string = document.getElementById("dashBoardsSearchBar").value
-        for(let i =0;i<this.dashBoards.length;i++){
-            if(this.dashBoards[i].includes(string)){
+    filter() {
+        this.dashBoards = fs.readdirSync("dashBoards");
+        let dashBoardsList = [];
+        let string = document.getElementById("dashBoardsSearchBar").value;
+        for (let i = 0; i < this.dashBoards.length; i++) {
+            if (this.dashBoards[i].includes(string)) {
                 dashBoardsList.push(this.dashBoards[i]);
             }
         }
-        this.postList(dashBoardsList)
+        this.postList(dashBoardsList);
     }
-    openNewDashBoard(dashBoardName){
-        mainwindow.dispatchEvent('LoadNewDashBoard',{'name':dashBoardName,'context': 'start_show'})
-        document.getElementById("dashBoardsSearchBar").value = ""
-        mainwindow.dispatchEvent('ClosePopupMenu');
+    openNewDashBoard(dashBoardName) {
+        mainwindow.dispatchEvent("LoadNewDashBoard", {
+            "name": dashBoardName,
+            "context": "start_show"
+        });
+        document.getElementById("dashBoardsSearchBar").value = "";
+        mainwindow.dispatchEvent("ClosePopupMenu");
     }
-    postList(dashBoardsList){
-        let htmlList = ""
-        for(let i=0;i<dashBoardsList.length;i++){
-            htmlList +=`<button 
+    postList(dashBoardsList) {
+        let htmlList = "";
+        for (let i = 0; i < dashBoardsList.length; i++) {
+            htmlList += `<button 
                         onclick="mainwindow.popUpMenu.menus['DashBoardsList'].openNewDashBoard('${dashBoardsList[i]}')">
                         ${dashBoardsList[i]}
-                        </button>`
+                        </button>`;
         }
-        document.getElementById("SavedDashBoardsList").innerHTML = htmlList
+        document.getElementById("SavedDashBoardsList").innerHTML = htmlList;
     }
-    htmlBuilder(){
-    return  `<div class="tabcontent" id="DashBoardsList">
+    htmlBuilder() {
+        return `<div class="tabcontent" id="DashBoardsList">
                 <input type="text" id="dashBoardsSearchBar" onkeyup="mainwindow.popUpMenu.menus['DashBoardsList'].filter()" placeholder="Search by name">
                 <div id="SavedDashBoardsList">
                 </div>
-            </div>`
+            </div>`;
     }
-}
+};
